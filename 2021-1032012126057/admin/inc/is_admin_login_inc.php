91942bfb9cb6a642abae969d23893c94e948d829
1474855195 <96041025+1474855195@users.noreply.github.com>
2022-08-29 13:21:13 +0800

    Delete 2021-1032012126057 directory

--- 2021-1032012126057/admin/inc/is_admin_login_inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-if(!is_admin_login($link)){
-    skip('login.php','error','请先登录在进行操作！');
-}
-?>
--- 2021-1032012126057/front/inc/front_footer_inc.php
+++
@@ -1,8 +0,0 @@
-<div id="footer" class="auto">
-		<div class="bottom">
-			<a>17智12苏浩</a>
-		</div>
-		<div class="copyright" ><p font-size=25px>学生综合素质评测系统</p></div>
-	</div>
-</body>
-</html>
